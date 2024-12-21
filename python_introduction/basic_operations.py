b87c4486c1b5ac68284ebce449b9173ab1306131
Chris-Adai <christopheradai.ca@gmail.com>
2024-12-21 19:29:02 +0100

    Update basic_operations.py
    had to modify the spaces around the equality sign

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,14 @@
  # basic operations
 
-number1=10
+number1 = 10
 
-number2=5
+number2 = 5
 
-sum=number1 + number2
+sum = number1 + number2
 
-diff=number1-number2
+diff = number1-number2
 
-product=number1*number2
+product = number1*number2
 
 print ("Addition of ", number1, " and ", number2, " is ", sum)
 #Addition of  10  and  5  is  15
