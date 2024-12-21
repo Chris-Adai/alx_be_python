06a0ddac63a6c5d19dd7b918587b508078464953
Chris-Adai <christopheradai.ca@gmail.com>
2024-12-21 20:21:24 +0100

    Update hours_to_seconds.py
    I changed the space around the equal sign. and removed the >>> sign

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,6 @@
->>> hours=2
->>> seconds= 3600 * hours
->>> print (hours," hour(s) is ", seconds, " seconds")
-2  hour(s) is  7200  seconds
->>>
+hours=2
+
+seconds= 3600 * hours
+
+print (hours," hour(s) is ", seconds, " seconds")
+# 2  hour(s) is  7200  seconds
