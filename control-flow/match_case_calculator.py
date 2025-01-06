0ef7a8244661fd2cca3638613b16a7166fc546f9
Chris-Adai <christopheradai.ca@gmail.com>
2025-01-07 00:05:42 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -8,21 +8,24 @@ match operation:
     case "+":
         num3 = num1 + num2
         # print ("num1 + num2 = ", num3)
-        print ("The result is ", num3)
+        # print ("The result is ", num3)
+        print(f"The result is {result}")
         
 
     case "-":
         num3 = num1 - num2
         # print (num3)
         # print ("num1 - num2 = ", num3)
-        print ("The result is ", num3)
+        # print ("The result is ", num3)
+        print(f"The result is {result}")
 
 
     case "*":
         num3 = num1 * num2
         # print(num3)
         # print ("num1 * num2 = ", num3)
-        print ("The result is ", num3)
+        # print ("The result is ", num3)
+        print(f"The result is {result}")
 
 
 
@@ -30,10 +33,10 @@ match operation:
         num3 = num1/num2
         # print(num3)
         # print ("num1 / num2 = ", num3)
-        if num2 = 0:
+        if num2 == 0:
             print ("Cannot divide by zero. ")
         else:
-            print ("The result is ", num3)
-
+            # print ("The result is ", num3)
+            print(f"The result is {result}")
 
         
