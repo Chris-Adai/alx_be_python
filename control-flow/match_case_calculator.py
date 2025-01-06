aba28300e109e6f628b92c7e5d4329f0955ab47c
Chris-Adai <christopheradai.ca@gmail.com>
2025-01-06 23:50:05 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,18 +7,23 @@ operation = input("Choose the operation (+, -, *, /): " )
 match operation:
     case "+":
         num3 = num1 + num2
-        print ("num1 + num2 = ", num3)
+        # print ("num1 + num2 = ", num3)
+        print ("The result is ", num3)
+        
 
     case "-":
         num3 = num1 - num2
         # print (num3)
-        print ("num1 - num2 = ", num3)
+        # print ("num1 - num2 = ", num3)
+        print ("The result is ", num3)
 
 
     case "*":
         num3 = num1 * num2
         # print(num3)
-        print ("num1 * num2 = ", num3)
+        # print ("num1 * num2 = ", num3)
+        print ("The result is ", num3)
+
 
 
     case "/":
