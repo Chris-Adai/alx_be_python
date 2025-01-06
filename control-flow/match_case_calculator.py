5f817a638b914ba1fe97931a002aff22ec31057d
Chris-Adai <christopheradai.ca@gmail.com>
2025-01-06 23:50:48 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -29,6 +29,7 @@ match operation:
     case "/":
         num3 = num1/num2
         # print(num3)
-        print ("num1 / num2 = ", num3)
+        # print ("num1 / num2 = ", num3)
+        print ("The result is ", num3)
 
         
