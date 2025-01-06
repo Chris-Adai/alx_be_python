726bdb8c4bf057717a3275cd3c3305412a424730
Chris-Adai <christopheradai.ca@gmail.com>
2025-01-07 00:00:01 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -30,6 +30,10 @@ match operation:
         num3 = num1/num2
         # print(num3)
         # print ("num1 / num2 = ", num3)
-        print ("The result is ", num3)
+        if num2 = 0:
+            print ("Cannot divide by zero. ")
+        else:
+            print ("The result is ", num3)
+
 
         
